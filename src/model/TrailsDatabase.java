d41e41c9498a1f743ab4f9340aae0b8a62dd8991
BuildTools <unconfigured@null.spigotmc.org>
2021-02-11 15:21:41 -0500

    Made adjustments to the key value in the TrailsDatabase.java to allow for multiple trails of the same name.

--- src/model/TrailsDatabase.java
+++ src/model/TrailsDatabase.java
@@ -3,6 +3,7 @@ package model;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.TreeSet;
 
 //Class for storing all the trails and their respective data.
 public class TrailsDatabase implements Serializable {
@@ -10,12 +11,16 @@ public class TrailsDatabase implements Serializable {
     private static final long serialVersionUID = -2223849863310362697L;
     //The string(key) will be the trail name;
     private HashMap<String, Trail> trailDatabase;
+    private TreeSet<Trail> trails;
+    private int idCounter = 0; //Each time a trail is added this id will first increase by 1
 
     public TrailsDatabase(){
         trailDatabase = new HashMap<String, Trail>(50000, .5f);
+        trails = new TreeSet<Trail>();
         addTrail(new Trail("Test Trail", "5 Test Ave., NY", 5, 2, TrailDifficulty.EASY, TrailType.LOOP));
         addTrail(new Trail("A Trail", "52 Test Ave., NY", 8, 10, TrailDifficulty.MODERATE, TrailType.LOOP));
         DataSaver.saveTrailsData(this);
+
     }
 
     public HashMap<String, Trail> getTrailDatabase(){
@@ -35,12 +40,9 @@ public class TrailsDatabase implements Serializable {
         return false;
     }
 
-    public boolean addTrail(Trail trail){
-        if(!trailDatabase.containsKey(trail.getTrailName())){
-            trailDatabase.put(trail.getTrailName(), trail);
-            return true;
-        }
-        return false;
+    public void addTrail(Trail trail){
+        idCounter++; //increase id by 1
+        trailDatabase.put(String.valueOf(idCounter), trail);
     }
 
     public boolean removeTrail(String trailName){
